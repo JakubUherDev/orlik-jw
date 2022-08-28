ead1c83d343948897657a92e37a78b26d09e15c6
JakubUherDev <jjuherdev@gmail.com>
2022-08-28 11:34:39 +0100

    Update: Images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import logoUrl from '../../assets/react.svg';
+import logoUrl from '../../assets/logo.png';
 const Header = () => (
     <header>
         <nav
@@ -10,24 +10,8 @@ const Header = () => (
                 <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
                     <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
                         <li className={'mx-auto'}>
-                            <img src='public/logo.png' alt={'KotowscyDent logo - Orlik JW'}/>
+                            <img src={'src/assets/logo.png'} alt={'KotowscyDent logo - Orlik JW'}/>
                         </li>
-                        {/*<li className="nav-item">*/}
-                        {/*    <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"*/}
-                        {/*       href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Home</a>*/}
-                        {/*</li>*/}
-                        {/*<li className="nav-item">*/}
-                        {/*    <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"*/}
-                        {/*       href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Features</a>*/}
-                        {/*</li>*/}
-                        {/*<li className="nav-item">*/}
-                        {/*    <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"*/}
-                        {/*       href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Pricing</a>*/}
-                        {/*</li>*/}
-                        {/*<li className="nav-item mb-2 lg:mb-0">*/}
-                        {/*    <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"*/}
-                        {/*       href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">About</a>*/}
-                        {/*</li>*/}
                     </ul>
                 </div>
             </div>
